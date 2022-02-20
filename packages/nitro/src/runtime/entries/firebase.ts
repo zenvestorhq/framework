dcce59150b53977d60449b31ee2f4d8929246a12
Ramesh Kumar <rameshkumar@Rameshs-MacBook-Pro.local>
2022-02-20 22:19:55 +0530

    support for firebase region and memory options

--- packages/nitro/src/runtime/entries/firebase.ts
+++ packages/nitro/src/runtime/entries/firebase.ts
@@ -1,7 +1,17 @@
-import '#polyfill'
+import "#polyfill";
 
 // @ts-ignore
-import functions from 'firebase-functions'
-import { handle } from '../server'
+import functions from "firebase-functions";
+import { handle } from "../server";
 
-export const server = functions.https.onRequest(handle)
+let firebaseFunction = functions;
+if (process.env.FB_REGION) {
+  firebaseFunction = firebaseFunction.region(process.env.FIREBASE_REGION);
+}
+
+if (process.env.FB_MEMORY) {
+  firebaseFunction = firebaseFunction.runWith({
+    memory: process.env.FB_MEMORY,
+  });
+}
+export const server = firebaseFunction.https.onRequest(handle);
